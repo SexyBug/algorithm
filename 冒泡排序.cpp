e1f810969bf511b8e3df19f4ca813322156abed5
SexyBug <2921978909@qq.com>
2021-12-02 17:22:37 +0800

    sort

---
+++ 冒泡排序.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	vector<int> v;
+	int num;
+	while (cin >> num)
+	{
+		if (num == 0) {
+			break;
+		}
+		v.push_back(num);
+	}
+
+	bool hasSwap = true;
+	for (int i = v.size() - 1; i > 0 && hasSwap; --i)
+	{
+		for (int j = 0; j <= i-1; ++j)
+		{
+			hasSwap = false;
+			if (v[j] > v[j + 1])
+			{
+				hasSwap = true;
+				swap(v[j], v[j + 1]);
+			}
+		}
+	}
+
+	for (int k = 0; k < v.size(); ++k) {
+		cout << v[k] << ",";
+	}
+	cout << endl;
+}
---
+++ 选择排序.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main() 
+{
+	vector<int> v;
+	int num;
+	while (cin>>num) 
+	{
+		if (num == 0) {
+			break;
+		}
+		v.push_back(num);
+	}
+
+	for (int i = 0; i < v.size();++i) 
+	{
+		int min = i;
+		for (int j = i + 1; j < v.size(); ++j) 
+		{
+			if (v[j] < v[min]) 
+			{
+				min = j;
+			}
+		}
+		swap(v[i],v[min]);
+	}
+
+	for (int k = 0; k < v.size(); ++k) 
+	{
+		cout << v[k] << ",";
+	}
+	cout << endl;
+}
